cce3d4b76d6714522d8a519fc447dbe2afa70a34
Farok Hossain <farokafs7@gmail.com>
2024-02-24 10:19:28 +0600

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,11 +11,7 @@ const Login = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center md:w-1/2 lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+          <p className="py-6">please order some sufer delicious food</p>
         </div>
         <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleLogin} className="card-body">
