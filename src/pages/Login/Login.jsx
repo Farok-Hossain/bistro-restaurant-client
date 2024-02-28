a1010d0ec6972c5450c521120db6cb3b03d45b35
Farok Hossain <farokafs7@gmail.com>
2024-02-28 21:29:41 +0600

    login form updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -11,7 +11,9 @@ const Login = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center md:w-1/2 lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">please order some sufer delicious food</p>
+          <p className="py-6">
+            click here and please order some sufer delicious food
+          </p>
         </div>
         <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
           <form onSubmit={handleLogin} className="card-body">
