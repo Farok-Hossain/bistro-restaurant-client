32634e946f09954199949f7ec57dcd3502f58c10
Farok Hossain <farokafs7@gmail.com>
2024-03-17 17:43:14 +0600

    axiosSecure updated

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 
 const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
   // request interceptor to add authorization header for every secure call to the api
   axiosSecure.interceptors.request.use(
     function (config) {
@@ -23,9 +27,14 @@ const useAxiosSecure = () => {
     function (response) {
       return response;
     },
-    (error) => {
+    async (error) => {
       const status = error.response.status;
       console.log("status error in the interceptor", status);
+      // for 401 or 403 logout the user and move the user to the login
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
       return Promise.reject(error);
     }
   );
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -70,7 +70,7 @@ const AuthProvider = ({ children }) => {
     return () => {
       return unsubscribe();
     };
-  }, []);
+  }, [axiosPublic]);
 
   const authInfo = {
     user,
