a81d046d0ee28dd964632ea05489680d1d07497d
Farok Hossain <farokafs7@gmail.com>
2024-03-20 03:14:03 +0600

    payment updated

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,6 +1,8 @@
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
+  const [error, setError] = useState("");
   const stripe = useStripe();
   const elements = useElements();
   const handleSubmit = async (event) => {
@@ -13,6 +15,18 @@ const CheckoutForm = () => {
 
     if (card === null) {
       return;
+    }
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
+    if (error) {
+      console.log("payment error", error);
+      setError(error.message);
+    } else {
+      console.log("payment method", paymentMethod);
+      setError("");
     }
   };
 
@@ -34,9 +48,14 @@ const CheckoutForm = () => {
           },
         }}
       />
-      <button type="submit" disabled={!stripe}>
+      <button
+        className=" btn btn-sm btn-primary my-4"
+        type="submit"
+        disabled={!stripe}
+      >
         Pay
       </button>
+      <p className=" text-red-600">{error}</p>
     </form>
   );
 };
--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,10 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
 // TODO: add publishable key
-const stripePromise = loadStripe("");
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
   return (
@@ -14,7 +15,9 @@ const Payment = () => {
       ></SectionTitle>
 
       <div>
-        <Elements stripe={stripePromise}></Elements>
+        <Elements stripe={stripePromise}>
+          <CheckoutForm></CheckoutForm>
+        </Elements>
       </div>
     </div>
   );
