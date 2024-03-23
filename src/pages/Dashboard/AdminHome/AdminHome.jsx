2b2c48369022d6292bbc23ffdfeba87ac0bc9ea2
Farok Hossain <farokafs7@gmail.com>
2024-03-23 23:34:22 +0600

    updated adminHome

--- src/pages/Dashboard/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/AdminHome/AdminHome.jsx
@@ -1,13 +1,77 @@
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { FaDollarSign } from "react-icons/fa";
 
 const AdminHome = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: stats } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
   return (
     <div>
       <h1 className=" text-3xl">
-        <span>Hi, Welcome</span>
+        <span>Hi, Welcome </span>
         {user?.displayName ? user.displayName : "Back"}
       </h1>
+      <div className="stats shadow">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-3xl"></FaDollarSign>
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stats.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">New Users</div>
+          <div className="stat-value">4,200</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-8 h-8 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
+              ></path>
+            </svg>
+          </div>
+          <div className="stat-title">New Registers</div>
+          <div className="stat-value">1,200</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
     </div>
   );
 };
