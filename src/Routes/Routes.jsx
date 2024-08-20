a7cb377a310e36b3647dace8603a76a39dd3ca91
Farok Hossain <farokafs7@gmail.com>
2024-08-21 00:58:51 +0600

    updated

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -137,7 +137,9 @@ export const router = createBrowserRouter([
         ),
 
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(
+            `https://bistro-restaurant-server-puce.vercel.app/menu/${params.id}`
+          ),
       },
     ],
   },
--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -12,7 +12,7 @@ import "@smastrom/react-rating/style.css";
 const Testimonials = () => {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/reviews")
+    fetch("https://bistro-restaurant-server-puce.vercel.app/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
